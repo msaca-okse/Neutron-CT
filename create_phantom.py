8062fcf94bf89654503dd1e31819bf9da13874e4
Martin Sæbye Carøe <msaca@dtu.dk>
2024-10-02 09:37:05 +0200

    Added script to simulate phantoms, and to process the CT data

--- create_phantom.py
+++ create_phantom.py
@@ -4,23 +4,55 @@ from cil.framework import ImageGeometry, ImageData, AcquisitionGeometry, Acquisi
 from cil.utilities.display import show2D
 import matplotlib.pyplot as plt
 from cil.plugins.astra import ProjectionOperator
+from astropy.io import fits
+
+
+def circular_mask(shape, r):
+    """
+    Create a mask for a quadratic 2D array with ones inside a circle and zeros outside.
+    
+    Parameters:
+    shape (tuple): Shape of the quadratic array (n, n).
+    r (float): Radius for the mask. r=1 means all values are set to one, r=0 means all values are set to zero.
+    
+    Returns:
+    numpy.ndarray: Mask array with ones inside the circle and zeros outside.
+    """
+    n = shape[0]
+    
+    # Create a grid of coordinates
+    y, x = np.ogrid[:n, :n]
+    
+    # Calculate the center of the array
+    center = (n - 1) / 2  # Works for both even and odd dimensions
+    
+    # Calculate the distance of each point from the center
+    distance_from_center = np.sqrt((x - center)**2 + (y - center)**2)
+    
+    # Create the mask: values inside the circle (<=r*n/2) are 1, outside are 0
+    mask = distance_from_center <= r * (n / 2)
+    
+    return mask.astype(np.uint8)
 
 # Create a 3D Shepp-Logan phantom
-def create_3d_shepp_logan(n_slice=128, size_x=256, size_y=256):
+def create_3d_shepp_logan(n_slice=128, size_x=256, size_y=256,flat=False):
 
+    if flat:
+        ellipsoids = []
+    else:
     # Parameters for the ellipsoids in the Shepp-Logan phantom
-    ellipsoids = [
-        # (value, a, b, c, x0, y0, z0, phi, theta, psi)
-        [10, 0.69*0.8, 0.92*0.8, 0.9*0.8, 0, 0, 0, 0, 0, 0],        # large ellipse
-        [8, 0.6624*0.8, 0.874*0.8, 0.88*0.8, 0, 0, 0, 0, 0, 0],     # inner ellipse
-        [5, 0.31, 0.11, 0.22, 0.22, 0, 0, -18, 0, 10],  # top right ellipse
-        [5, 0.41, 0.16, 0.15, -0.22, 0, 0, 18, 0, 10],  # top left ellipse
-        [6, 0.25, 0.21, 0.25, 0, 0.35, -0.15, 0, 0, 0], # bottom middle
-        [3, 0.046, 0.046, 0.046, 0, 0.1, 0.25, 0, 0, 0], # small middle
-        [4, 0.046, 0.023, 0.02, -0.08, -0.605, 0, 0, 0, 0], # bottom left
-        [4, 0.023, 0.023, 0.02, 0, -0.605, 0, 0, 0, 0],   # bottom middle
-        [4, 0.023, 0.046, 0.02, 0.06, -0.605, 0, 0, 0, 0], # bottom right
-    ]
+        ellipsoids = [
+            # (value, a, b, c, x0, y0, z0, phi, theta, psi)
+            [4, 0.69*0.65, 0.92*0.65, 0.9*0.65, 0, 0, 0, 0, 0, 0],        # large ellipse
+            [3, 0.6624*0.65, 0.874*0.65, 0.88*0.65, 0, 0, 0, 0, 0, 0],     # inner ellipse
+            [2, 0.31, 0.11, 0.22, 0.22, 0, 0, -18, 0, 10],  # top right ellipse
+            [2, 0.41*0.8, 0.16, 0.15, -0.22, 0, 0, 18, 0, 10],  # top left ellipse
+            [1.5, 0.25, 0.21, 0.25, 0, 0.35, -0.15, 0, 0, 0], # bottom middle
+            [0.5, 0.046, 0.046, 0.046, 0, 0.1, 0.25, 0, 0, 0], # small middle
+            [1, 0.046, 0.023, 0.02, -0.08, -0.605, 0, 0, 0, 0], # bottom left
+            [1, 0.023, 0.023, 0.02, 0, -0.605, 0, 0, 0, 0],   # bottom middle
+            [1, 0.023, 0.046, 0.02, 0.06, -0.605, 0, 0, 0, 0], # bottom right
+        ]
 
     # Create a 3D grid of coordinates
     x = np.linspace(-1, 1, size_x)
@@ -56,41 +88,88 @@ def create_3d_shepp_logan(n_slice=128, size_x=256, size_y=256):
     return out
 
 
-def simulate_projections(image_data, angles, noise_level = 0, beam_radius = 1):
-
+def simulate_projections(image_data, angles, beam_radius = None,dose = None,intensity = 1e4,dose_variation = 0.1):
     
-
-
     ig = image_data.geometry
     size_x = ig.voxel_num_x
     size_y = ig.voxel_num_y
-    ag = AcquisitionGeometry.create_Parallel3D(ray_direction=[0, 1, 0], detector_position=[0, 0, 0], 
-                                            detector_direction_x=[1, 0, 0], detector_direction_y=[0, 0, 1],
-                                            rotation_axis_position=[0, 0, 0], rotation_axis_direction=[0, 0, 1],
-                                            units='units distance')\
-                                            .set_angles(angles)\
-                                            .set_panel((size_x,size_y),pixel_size=(1/size_x,1/size_y))\
-                                            .set_labels(['vertical','angle', 'horizontal'])
-    radon = ProjectionOperator(image_geometry=ig, acquisition_geometry=ag, device='gpu')
-    out = radon.direct(image_data)
-    return out
+    if dose == None:
+        dose = np.random.uniform(low = -dose_variation, high = dose_variation, size=len(angles)) + np.linspace(20, 80, num=len(angles))
+    elif dose == 1:
+        dose = np.ones(len(angles))
 
-# 
+    if beam_radius == None:
+        mask = np.ones((size_x,size_y))
+    else:
+        mask = circular_mask((size_x, size_y), beam_radius)
+    
+    ag = AcquisitionGeometry.create_Parallel3D(ray_direction=[0, 1, 0], detector_position=[0, 0, 0], 
+                                        detector_direction_x=[1, 0, 0], detector_direction_y=[0, 0, 1],
+                                        rotation_axis_position=[0, 0, 0], rotation_axis_direction=[0, 0, 1],
+                                        units='units distance')\
+                                        .set_angles(angles)\
+                                        .set_panel((size_x,size_y),pixel_size=(1/size_x,1/size_y))\
+                                        .set_labels(['vertical','angle', 'horizontal'])
+    
+    radon = ProjectionOperator(image_geometry=ig, acquisition_geometry=ag, device='gpu')
+    noiseless_data = radon.direct(image_data)
+
+
+    noiseless_data = noiseless_data.sapyb(-1,ag.allocate(1),0.1)
+
+    noiseless_data = noiseless_data.exp() # Get the exponential data...
+
+    noisy_data = noiseless_data.clone()
+    # Add dose over each projection (angle)
+    
+
+    # Reshape the 1D array to broadcast along the second dimension
+    dose = dose[np.newaxis, :, np.newaxis]  # Shape: (1, 256, 1)
+    # Multiply the 1D array across the 3D array along the second axis
+    noiseless_data.fill(noiseless_data.as_array() * dose)
+    #poisson_noise.fill(np.random.poisson(poisson_noise_level, size=(size_x, len(angles),size_y)))
+    noisy_data.fill(np.random.poisson(intensity*noiseless_data.as_array()))
+    noiseless_data.fill(noiseless_data.as_array()*intensity)
+    # Rescale to uint (divided by 2)
+    scale = np.max(noisy_data.as_array())
+    noisy_data = noisy_data.sapyb(65535/2/scale,ag.allocate(1),0)
+    noiseless_data = noiseless_data.sapyb(65535/2/scale,ag.allocate(1),0)
+
+
+    noisy_data.fill(noisy_data.as_array().astype(np.uint16)*mask[:,np.newaxis,:]+1)
+    noiseless_data.fill(noiseless_data.as_array().astype(np.uint16)*mask[:,np.newaxis,:]+1)
+
+    
+    return noisy_data, noiseless_data
+
 
 
 
 if __name__=='__main__':
     size_x, size_y, n_slice = 256, 256, 128  # Custom size: 256x256x128
-    phantom_3d = create_3d_shepp_logan(n_slice = n_slice,size_x=size_x, size_y=size_y)
+    phantom_3d = create_3d_shepp_logan(n_slice = n_slice,size_x=size_x, size_y=size_y,flat=True)
     phantom_3d.reorder('astra')
     
-    print(phantom_3d)
+    angles = np.linspace(0, 360, 100, endpoint=False, dtype=np.float32)
+    angles = np.repeat(angles,3) # Take 3 measurements per angle.
 
-    angles = np.linspace(0, 360, 10, endpoint=False, dtype=np.float32)
-
-    
-    data = simulate_projections(phantom_3d, angles)
+    ndata, data = simulate_projections(phantom_3d, angles, dose = None,dose_variation=5,intensity=1e2,beam_radius=0.85)
+    ndata.reorder(('angle','horizontal','vertical'))
+    data.reorder(('angle','horizontal','vertical'))
+    print(ndata)
     print(data)
-    show2D(data)
-    plt.savefig('/work3/msaca/current_figures/current.png')
+    current_image = show2D(data.log(),slice_list = ('angle',120))
+    current_image.save('/work3/msaca/current_figures/current.png')
+    current_image = show2D(ndata.log(),slice_list = ('angle',120))
+    current_image.save('/work3/msaca/current_figures/current2.png')
+    
+    path1 = '/work3/msaca/simulation_data/experiment_1/flats/'
 
+    save = True
+    if save == True:
+        ndata = ndata.as_array()
+        for i_angle in range(len(angles)):
+            # Create a FITS file for each slice
+            hdu = fits.PrimaryHDU(ndata[i_angle])  # Create a PrimaryHDU object with the 2D slice
+            filename = 'flat_simx3_'+ f'{i_angle+1:05}.fits'  # Filename for each FITS file
+            hdu.writeto(path1 + filename, overwrite=False) 
---
+++ preprocess_data.py
@@ -0,0 +1,88 @@
+# Goal ofthis scriptis to handle the 3D volume as individual 2D slices. The projections are normalized (flat + dose corrected), then loaded in smaller batches to save memory and then saved as sinograms.
+# The projections are NOT dark current corrected.
+
+## Select packages
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from cil.processors import Normaliser
+from cil.framework import AcquisitionGeometry, AcquisitionData
+from cil.utilities.display import show2D, show_geometry
+from cil.io import TIFFWriter
+
+# Specify settings
+upper_path = '/work3/msaca/simulation_data/experiment_1/'
+subfolder_projections = 'projections/'
+subfolder_flats = 'flats/'
+subfolder_normalizations = 'normalizations/'
+filename_prefix_projections = 'proj_simx3_'
+filename_prefix_flats = 'flat_simx3_'
+filename_suffix = '.fits'
+N_pixels = 256
+OB_reg_TL = (150, 150)
+OB_reg_BR = (170 , 170)
+
+N_OB = 300 # Number of flats
+file_count = 0 # flat index offset
+OB = np.zeros((N_pixels, N_pixels))
+for i_OB in range(N_OB):
+    file_count = file_count + 1
+    path_load_OB = upper_path + subfolder_flats + filename_prefix_flats + f"{file_count:05}" + '.fits'
+    OB = OB + fits.open(path_load_OB)[0].data/N_OB # Load data
+    if not np.mod(file_count,50):
+        string_progress = '. Loaded file ' + str(file_count) + ' out of ' + str(N_OB)
+        print(string_progress)
+print('Done filtering OB-files')
+
+D0 = np.mean(OB[OB_reg_TL[0]:OB_reg_BR[0], OB_reg_TL[1]:OB_reg_BR[1]])
+OB_mean = OB
+
+# Settings for the projection file loader
+N_slices = N_pixels
+N_projections = 100 # Number of projections
+batch_size = 32
+N_batches = int(np.ceil(N_slices/batch_size))
+
+D = np.zeros((N_projections))
+OB_mean = np.clip(OB_mean,1,1000000) # Make sure there are no zero or negative values.
+for i_batch in range(N_batches):
+    if i_batch == N_batches - 1:
+        updated_batch_size = N_slices - (N_batches - 1)*batch_size
+    else:
+        updated_batch_size = batch_size
+    # Set the geometry. Data is saved as 2D slices...
+    ag = AcquisitionGeometry.create_Parallel3D()  \
+    .set_panel(num_pixels=[N_pixels,updated_batch_size])        \
+    .set_angles(angles=np.linspace(0,360,N_projections,endpoint=False))
+    ag.set_labels(['angle','horizontal','vertical'])
+    i_from = i_batch*batch_size
+    i_to = i_batch*batch_size+updated_batch_size
+
+    OB_batch = OB_mean[:,i_from:i_to] # Select the OB means for the current batch
+
+    A = np.zeros((N_projections, N_pixels,updated_batch_size)) # Allocate sinograms
+    file_count = 0
+    for i_proj in range(N_projections):
+        file_count = file_count + 1
+        path_1 = upper_path + subfolder_projections + filename_prefix_projections + f"{(3*file_count-2):05}" + filename_suffix
+        path_2 = upper_path + subfolder_projections + filename_prefix_projections + f"{(3*file_count-1):05}" + filename_suffix
+        path_3 = upper_path + subfolder_projections + filename_prefix_projections + f"{3*file_count:05}" + filename_suffix
+
+        if not np.mod(file_count,50):
+            string_progress = 'Batch ' + str(i_batch+1) + ' out of ' + str(N_batches) + '. Loaded file ' + str(file_count) + ' out of ' + str(N_projections)
+            print(string_progress)
+
+        projection1 = fits.open(path_1)[0].data
+        projection2 = fits.open(path_2)[0].data
+        projection3 = fits.open(path_3)[0].data
+        projection_mean = (projection1 + projection2 + projection3)/3
+        D[i_proj] = np.mean(projection_mean)
+        projection_mean_batch = projection_mean[:,i_from:i_to]
+        projection_lognormalized = -(np.log(projection_mean_batch) - np.log(OB_batch) + np.log(D0) - np.log(D[i_proj]))
+
+        A[i_proj] = projection_lognormalized
+
+    A_slices = AcquisitionData(A, geometry=ag, deep_copy=False)
+    A_slices.reorder(('vertical', 'angle','horizontal'))
+    A_write = TIFFWriter(data=A_slices, file_name = upper_path + subfolder_normalizations + 'sinogram_normalized', counter_offset=i_batch*batch_size+1)
+    A_write.write()
